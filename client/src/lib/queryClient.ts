5a7fae19b208301e5c1b4f51e38612156b0a7493
os6s <223287884+os6s@users.noreply.github.com>
2025-08-09 22:57:36 +0300

    Update queryClient.ts

--- client/src/lib/queryClient.ts
+++ client/src/lib/queryClient.ts
@@ -11,7 +11,7 @@ export async function apiRequest(
   method: string,
   url: string,
   data?: unknown | undefined,
-): Promise<Response> {
+): Promise<any> {  // ترجع بيانات JSON مش Response
   const res = await fetch(url, {
     method,
     headers: data ? { "Content-Type": "application/json" } : {},
@@ -20,7 +20,8 @@ export async function apiRequest(
   });
 
   await throwIfResNotOk(res);
-  return res;
+
+  return await res.json();  // هنا ترجّع البيانات فعلياً
 }
 
 type UnauthorizedBehavior = "returnNull" | "throw";
