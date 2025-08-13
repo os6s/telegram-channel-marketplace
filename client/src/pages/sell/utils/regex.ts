5b6169872259165cad2235bcba5790f9c2fd43a6
os6s <223287884+os6s@users.noreply.github.com>
2025-08-14 02:01:19 +0300

    Create regex.ts

---
+++ client/src/pages/sell/utils/regex.ts
@@ -0,0 +1,5 @@
+export const RE_PRICE = /^\d+(\.\d{1,9})?$/;
+export const RE_TG_USER = /^[a-z0-9_]{4,15}$/;      // تيليجرام بدون .
+export const RE_GENERIC = /^[a-z0-9._]{2,15}$/;      // تويتر/انستا/ديسكورد/سناب
+export const RE_TIKTOK = /^[a-z0-9._]{3,15}$/;       // تيك توك 3–15
+export const RE_MONTH = /^\d{4}-(0[1-9]|1[0-2])$/;   // YYYY-MM
