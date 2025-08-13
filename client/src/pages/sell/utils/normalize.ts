538dc532b3f045ddb2db1035df17b3b12e6e9d48
os6s <223287884+os6s@users.noreply.github.com>
2025-08-14 02:00:39 +0300

    Create normalize.ts

---
+++ client/src/pages/sell/utils/normalize.ts
@@ -0,0 +1,7 @@
+export const normalizeUsername = (s: string) =>
+  String(s || "")
+    .trim()
+    .replace(/^@/, "")
+    .replace(/^https?:\/\/t\.me\//i, "")
+    .replace(/^t\.me\//i, "")
+    .toLowerCase();
