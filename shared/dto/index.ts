f453934b6895b05ddfd2a172b2fe1427d25eaec6
os6s <223287884+os6s@users.noreply.github.com>
2025-08-24 16:55:58 +0300

    Create index.ts

---
+++ shared/dto/index.ts
@@ -0,0 +1,9 @@
+// shared/dto/index.ts
+
+export * from "./users";
+export * from "./listings";
+export * from "./payments";
+export * from "./activities";
+export * from "./disputes";
+export * from "./payouts";
+export * from "./wallet";
