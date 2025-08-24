e78c83bbb5de6454a06879612ac15ad8fd7443a5
os6s <223287884+os6s@users.noreply.github.com>
2025-08-24 15:09:31 +0300

    Create index.ts

---
+++ shared/schema/index.ts
@@ -0,0 +1,12 @@
+export * from "./enums";
+export * from "./users";
+export * from "./listings";
+export * from "./payments";
+export * from "./disputes";
+export * from "./activities";
+export * from "./payouts";
+export * from "./views";
+export * from "./relations";
+export * from "./zod";
+
+export type { InferInsertModel as InsertModel, InferSelectModel as SelectModel } from "drizzle-orm";
